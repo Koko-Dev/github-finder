7e8dadd8129d7ab7d3f83ce3653389899cbffe22
Koko-Dev <koko.webdev@gmail.com>
2022-10-30 19:02:45 -0400

    Navbar.jsx: Import the github icon from react-icons/fa;  import Link from react-router-dom; import PropTypes from prop-types;  Add the Navabar title proptype as a string and the Navbar default prop title whose initial value will be 'Github Finder'

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,20 @@
+import {FaGithub} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+
 function Navbar() {
   return (
     <div>Navbar</div>
   )
+}
+
+Navbar.defaultProps = {
+  title: 'Github Finder'
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string
 }
 
 export default Navbar
