1e60f5aeb0f1958b75369c76335658717986763a
Koko-Dev <koko.webdev@gmail.com>
2022-10-30 20:16:21 -0400

    Navbar.jsx: Add Github icon with props title; use classes from daisyui and tailwind for styling

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,7 +5,17 @@ import PropTypes from 'prop-types'
 
 function Navbar({ title }) {
   return (
-    <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>Navbar</nav>
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+     <div className="container mx-auto">
+       <div className="fex-none px-2 mx-2">
+         <FaGithub className='inline pr-2 text-3xl'/>
+         <Link to='/' className='text-lg font-bold align-middle'/>
+         { title }
+       </div>
+       {/* /.fex-none px-2 mx-2 */}
+     </div>
+     {/* /.container mx-auto */}
+    </nav>
   )
 }
 
