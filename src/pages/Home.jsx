0e4905e919ebf60c9e38f6ca9a9623956426e515
Koko-Dev <koko.webdev@gmail.com>
2022-10-30 21:34:43 -0400

    NotFound.jsx:  Add Content and styling;  Reformat

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ function Home() {
   return (
     <div>
       <h1 className="text-6xl">Welcome</h1>
-      {/* /.text-6xl */}
+      {/* /.text-6xl */ }
     </div>
-  )
+  );
 }
 
 export default Home;
