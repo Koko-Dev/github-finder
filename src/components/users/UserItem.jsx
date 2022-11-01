23f57d6202d273bb737b0140fa1c0c2da3725f2f
Koko-Dev <koko.webdev@gmail.com>
2022-11-01 10:31:41 -0400

    UserItem.jsx:  We have destructured the argument to extract the user prop; we want to further use destructuring to only extract the user's login and avatar_url;  in the UserItem argument, in place of just ( { user } ), use ( { user: { login, avatar_url } }); we do this because the user object coming in is very large and we only need two small things

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-function UserItem({ user }) {
+function UserItem({ user: { login, avatar_url } }) {
   return (
     <div className='card, shadow-md compact side bg-base-100'>
       <div className='flex-row items-center space-x-4 card-body'>
