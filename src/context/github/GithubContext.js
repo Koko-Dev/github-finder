77ed8d0e8fa9ff77740def11a1f37bff8fa90f11
Koko-Dev <koko.webdev@gmail.com>
2022-11-01 11:15:45 -0400

    GithubContext.js:  Import createContext and create const GithubContext which calls it;  bring in the Github URL and Github Token from .env; begin to export the GithubProvider with the argument of { children };  all of this is in preparation for bringing over state to context

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,11 @@
+import { createContext } from 'react';
+
+const GithubContext = createContext();
+
+// Github url and token
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+export const GithubProvider = ({ children }) => {
+
+};
