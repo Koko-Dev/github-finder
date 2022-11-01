0db4bd137df7653bbe8592428502b87f280dcfff
Koko-Dev <koko.webdev@gmail.com>
2022-11-01 10:38:58 -0400

    In place of { login }, add an img tag whose src is avatar_url; this is the Profile avatar of each Github account

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -6,7 +6,7 @@ function UserItem({ user: { login, avatar_url } }) {
       <div className="flex-row items-center space-x-4 card-body">
         <div className="avatar">
           <div className="rounded-full shadow w-14 h-14">
-            { login }
+            <img src={ avatar_url } alt="Profile"/>
           </div>
         </div>
       </div>
