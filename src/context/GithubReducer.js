23778e171d9fcb6f96a74a7601ed1ba18881d52c
Koko-Dev <koko.webdev@gmail.com>
2022-11-02 10:08:57 -0400

    Convert app from using useState to using reducders without a third party state library like redux; react has the useReducer hook which works very well for smaller apps
    1.  GithubContext.js:
         a.  Remove Import useState and instead import useReducer
         b.  Remove both state vars
         c.  Create a variable with the initalState of removed state vars users and loading; set as properties in an object whose values are the initalState -- users: [], loading: true
         d.  Create a function init which returns initial state value;  according to the docs, this is best practices and will be used as the third argument in useReducer
         e.  Import GithubReducer.js
         f.  In a destructured array whose consts are state and dispatch, set to call useReducer(githubReducer, initialState, init);
         g.  In fetchUsers() remove call to setUsers and setLoading since we are no longer using state;  instead, call dispatch whose argument is an object whose properties are type: 'GET_USERS', payload: { data };  in this way we have access to all the data from the fetch
         h.  In src/context/GithubReducer.js, create our reducer function;  when the action type is GET_USERS as defined in GithubContext.js fetchUsers() dispatch where we set the payload to contain the data from github containing users info, then we return {...state, users:action.payload.data, loading:false};
            -- if the action type is not 'GET USERS' then simply sent back state
         i.  In GithubContext.js, adjust users and loading to come from state and add the dispatch
    
    // GithubReducer.js
    
    export default const githubReducer = (state, action) => {
      switch (action.type) {
        case 'GET_USERS':
          return {
            ...state,
            users: action.payload.data,
            loading: false
          }
        default:
          return state;
      }
    };
    
    // GetContext.js
    import { createContext, useReducer } from 'react';
    import githubReducer from '../GithubReducer';
    
    export const GithubContext = createContext();
    
    const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
    const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
    
    export const GithubProvider = ({ children }) => {
      const initialState = {
        users: [],
        loading: true,
      };
    
      function init(initialState) {
        return {
          users: [],
          loading: true,
        }
      }
    
      const [state, dispatch] = useReducer(githubReducer, initialState, init);
    
      const fetchUsers = async () => {
        const response = await fetch(
          `${ GITHUB_URL }/users`,
          {
            headers: {
              Authorization: `token ${ GITHUB_TOKEN }`
            }
          });
    
        const data = await response.json();
        dispatch({
          type: 'GET_USERS',
          payload: {
            data,
          }
                 })
      };
    
      return (
        <GithubContext.Provider value={ {
          users: state.users,
          loading: state.loading,
          dispatch,
          fetchUsers
        } }>
          { children }
        </GithubContext.Provider>
      );
    };

---
+++ src/context/GithubReducer.js
@@ -0,0 +1,15 @@
+const githubReducer = (state, action) => {
+  switch (action.type) {
+    case 'GET_USERS':
+      return {
+        ...state,
+        users: action.payload.data,
+        loading: false
+      }
+    default:
+      return state;
+  }
+};
+
+
+export default githubReducer;
