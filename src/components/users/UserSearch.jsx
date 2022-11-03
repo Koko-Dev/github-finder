1a856f87f9a44c70e7896e61f924fef5762dd5ee
Koko-Dev <koko.webdev@gmail.com>
2022-11-03 10:26:28 -0400

    UserSearch.jsx:  In return div, add a button and a form input;  use tailwindcss and daisyui for styling

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,7 +1,26 @@
 function UserSearch() {
   return (
-    <div>User Search</div>
-  )
+    <div
+      className='grid grid-cols-1 xl:grid-cols-22 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8' >
+
+      <div >
+        <form action='' >
+          <div className='form-control' >
+            <div className='relative' >
+              <input
+                type='text'
+                className='w-full pr-40 bg-gray-200 input lg text-black'
+              />
+            </div >
+          </div >
+        </form >
+      </div >
+      <div >
+        <button className='btn btn-ghost btn-lg' >Clear</button >
+      </div >
+
+    </div >
+  );
 }
 
 
