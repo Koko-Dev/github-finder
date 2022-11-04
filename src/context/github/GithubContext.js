6aaa1b210eb5f010b81fe14fa2f71930ccb73887
Koko-Dev <koko.webdev@gmail.com>
2022-11-04 09:22:12 -0400

    Set up to search for github users
    GithubContext.js:
        -  Comment out fetchUsers() which we used for testing purposes
        -  In place of fetchUsers, create searchUsers(text) which will extract the parameter (say, q=brad ) and return the item array payload;
        -  Add searchUsers to the <GithubContext.Provider /> value prop and remove fetchUsers
    
    UserSearch.jsx:  Add searchUsers to useContext;  in place of handleSubmit(e) setText('') add searchUsers(text)
    
    This will not yet display the user on search but if we send a GET request to the endpoint https://api.github.com/search/users?q=brad it will show about 30 github user info of anyone whose username contains 'brad'

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -22,28 +22,50 @@ export const GithubProvider = ({ children }) => {
   const [ state, dispatch ] = useReducer(githubReducer, initialState, init);
 
   // Get initial users (testing purposes)
-  const fetchUsers = async () => {
+  // const fetchUsers = async () => {
+  //   setLoading();
+  //
+  //   const response = await fetch(`${ GITHUB_URL }/users`, {
+  //     headers: {
+  //       Authorization: `token ${ GITHUB_TOKEN }`
+  //     }
+  //   });
+  //
+  //   const data = await response.json();
+  //   dispatch({
+  //              type: 'GET_USERS',
+  //              payload: data,
+  //            });
+  // };
+
+  // Get search results
+  const searchUsers = async (text) => {
     setLoading();
 
-    const response = await fetch(`${ GITHUB_URL }/users`, {
-      headers: {
-        Authorization: `token ${ GITHUB_TOKEN }`
-      }
-    });
+    const params = new URLSearchParams({ q: text });
 
-    const data = await response.json();
+    const response = await fetch(
+      `${ GITHUB_URL }/users?${ params }`,
+      {
+        headers: {
+          Authorization: `token ${ GITHUB_TOKEN }`
+        }
+      });
+
+    const { items } = await response.json();
     dispatch({
                type: 'GET_USERS',
-               payload: data,
+               payload: items,
              });
   };
 
+
   // Set Loading
-  const setLoading = () => dispatch({type: 'SET_LOADING'})
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
   return (
     <GithubContext.Provider value={ {
-      users: state.users, loading: state.loading, dispatch, fetchUsers
+      users: state.users, loading: state.loading, dispatch, searchUsers
     } }>
       { children }
     </GithubContext.Provider>
