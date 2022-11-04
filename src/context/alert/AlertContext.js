7cf32ad5da75ee1be1d12afd6bacecc38c804ca3
Koko-Dev <koko.webdev@gmail.com>
2022-11-04 11:24:50 -0400

    AlertContext.js: Create a function, setAlert() that takes in a message and a type as arguments and dispatches a type: 'SET_ALERT' with message and type payload; make it so that after three seconds it also dispatches type 'REMOVE_ALERT'; add this function to the <AlertProvider /> value prop; remove dispatch from the value prop
    AlertReducer.js:  Create a function, AlertReducer(state, action) with a switch/case whose cases are action types;  if the action.type is 'GET_USERS' it should return the action.payload, which is the message and type in this case;  if the action.type is 'REMOVE_ALERT' it should simply return null;  set the default to return state

--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -12,9 +12,19 @@ export const AlertProvider = ({ children }) => {
 
   const [ state, dispatch ] = useReducer(alertReducer, initialState, init);
 
+  // Set an alert
+  const setAlert = (message, type) => {
+    dispatch({
+               type: 'SET_ALERT',
+               payload: { message, type }
+             });
+
+    setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }), 3000);
+  };
+
 
   return (
-    <AlertContext.Provider value={ { alert: state, dispatch } }>
+    <AlertContext.Provider value={ { alert: state, setAlert } }>
       { children }
     </AlertContext.Provider>
   );
