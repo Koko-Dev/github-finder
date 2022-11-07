9b3ca302b051a695761e01ee3947e50f8b561e3a
Koko-Dev <koko.webdev@gmail.com>
2022-11-07 11:51:52 -0500

    Get the user login name to display when the user clicks on 'Visit Profile' after a search
    User.jsx:
        -- Import useContext from react
        -- Import GithubContext
        -- Import userParams from react-router-dom
        -- Import Spinner from Spinner.jsx
        -- Use destructuring to extract getUser, user, loading and dispatch using useContext with argument GithubContext
        -- Create a var params that calls useParams() so that we can have access to any parameter in the url
        -- If loading is true, then return the Spinner component
        -- Change the User return div to display the user login name instead of just 'User'

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,7 +1,21 @@
+import { useContext } from 'react';
+import { GithubContext } from '../context/github/GithubContext';
+import { useParams } from 'react-router-dom';
+import Spinner from '../components/layout/Spinner';
+
 function User() {
+  const { getUser, user, loading, dispatch } = useContext(GithubContext);
+
+  const params = useParams();
+
+  if (loading) {
+    return <Spinner/>;
+  }
+
+
   return (
-    <div>User</div>
-  )
+    <div>{ params.login }</div>
+  );
 }
 
 export default User;
