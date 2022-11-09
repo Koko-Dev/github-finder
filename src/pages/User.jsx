373605b97264a28b7b162caf9baa7dda768c0e9e
Koko-Dev <koko.webdev@gmail.com>
2022-11-09 09:00:24 -0500

    User.jsx:  Import some fa icons from react icons;  import useContext and useEffect from react;  import Link from react-router-dom;  create a useEffect which only runs on load and calls getUser with login as the argument;  extract user properties;  create a link/button which takes us back home

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,6 +1,7 @@
-import { useContext } from 'react';
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
+import { useContext, useEffect } from 'react';
 import { GithubContext } from '../context/github/GithubContext';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Spinner from '../components/layout/Spinner';
 
 function User() {
@@ -8,13 +9,44 @@ function User() {
 
   const params = useParams();
 
+  useEffect(() => {
+    getUser(params.login)
+  }, [])
+
   if (loading) {
     return <Spinner/>;
   }
 
+  const {
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user
+
 
   return (
-    <div>{ params.login }</div>
+    <>
+      <div className='w-full mx-auto lg:w-10/12'>
+        <div className='mb-4'>
+          <Link
+            to='/'
+            className='btn btn-ghost'
+          >
+            Back to Search
+          </Link>
+        </div>
+      </div>
+    </>
   );
 }
 
