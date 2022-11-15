8c01821d90ebb7b822565545907af0c33ce57f64
Koko-Dev <koko.webdev@gmail.com>
2022-11-14 19:49:05 -0500

    GithubReducer.js: Refactor for readability

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -2,19 +2,26 @@ const githubReducer = (state, action) => {
   switch (action.type) {
     case 'GET_USERS':
       return {
-        ...state, users: action.payload, loading: false,
+        ...state,
+        users: action.payload,
+        loading: false,
       };
     case 'SET_LOADING':
       return {
-        ...state, loading: true,
+        ...state,
+        loading: true,
       };
     case 'CLEAR_USERS':
       return {
-        ...state, users: [],
+        ...state,
+        users: [],
       };
     case 'GET_USER_AND_REPOS':
       return {
-        ...state, user: action.payload.user, repos: action.payload.repos, loading: false,
+        ...state,
+        user: action.payload.user,
+        repos: action.payload.repos,
+        loading: false,
       };
     default:
       return state;
